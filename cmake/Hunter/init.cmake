e79ff8939de6d7ec85244991f40f5c53b5ed8f0d
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-09-08 12:00:18 +0300

    update: switch to hunter packet manager of Quadrivium
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/init.cmake
+++ cmake/Hunter/init.cmake
@@ -20,14 +20,14 @@ set(
 
 set(
     HUNTER_CACHE_SERVERS
-    "https://github.com/soramitsu/hunter-binary-cache"
+    "https://github.com/qdrvm/hunter-binary-cache;https://github.com/soramitsu/hunter-binary-cache"
     CACHE STRING "Binary cache server"
 )
 
 include(${CMAKE_CURRENT_LIST_DIR}/HunterGate.cmake)
 
 HunterGate(
-    URL  https://github.com/soramitsu/soramitsu-hunter/archive/v0.23.257-soramitsu21.tar.gz
-    SHA1 1366fb7be6bc0124b4fd54fa7aff32e99ff353ef
+    URL  https://github.com/qdrvm/hunter/archive/refs/tags/v0.23.257-qdrvm2.zip
+    SHA1 0b6628eb60053c91c025c9024fe6528bb19c094d
     LOCAL
 )
