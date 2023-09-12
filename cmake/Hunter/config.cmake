bab46bc7e370cd04fc43ed8e2755bc7841884f7c
Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>
2023-09-12 16:46:02 +0300

    update: hunter
    Signed-off-by: Dmitriy Khaustov aka xDimon <khaustov.dm@gmail.com>

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -4,14 +4,3 @@
 #    VERSION 0.0.0-package-version
 #    CMAKE_ARGS "CMAKE_VARIABLE=value"
 #)
-
-hunter_config(
-    fmt
-    URL  https://github.com/fmtlib/fmt/archive/refs/tags/10.1.1.tar.gz
-    SHA1 e94b38a9efe0d696373a3cb1300dd24f12e2dd9c
-    CMAKE_ARGS
-        FMT_DOC=OFF
-        FMT_TEST=OFF
-        FMT_USE_CPP14=OFF # don't force c++14
-    KEEP_PACKAGE_SOURCES
-)
